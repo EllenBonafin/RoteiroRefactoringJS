4d00a3fd68910eb6d66169334e27c5362331ab2d
Ellen <ellencarine41@gmail.com>
2023-07-30 21:30:20 -0300

    ok

--- index.js
+++ index.js
@@ -1,62 +1,10 @@
 const { readFileSync } = require('fs')
 
-function gerarFaturaStr(fatura, pecas) {
-  let totalFatura = 0
-  let creditos = 0
-  let faturaStr = `Fatura ${fatura.cliente}\n`
-  const formato = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-    minimumFractionDigits: 2
-  }).format
-
-  function getPeca(apresentacao){
-    return pecas[apresentacao.id]
-  }
-
-  for (let apre of fatura.apresentacoes) {
-    const peca = getPeca(apre)
-    let total = 0
-
-    switch (peca.tipo) {
-      case 'tragedia':
-        total = 40000
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30)
-        }
-        break
-      case 'comedia':
-        total = 30000
-        if (apre.audiencia > 20) {
-          total += 10000 + 500 * (apre.audiencia - 20)
-        }
-        total += 300 * apre.audiencia
-        break
-      default:
-        throw new Error(`Peça desconhecia: ${peca.tipo}`)
-    }
-
-    function calcularCredito(apre) {
-      let creditos = 0;
-      creditos += Math.max(apre.audiencia - 30, 0);
-      if (getPeca(apre).tipo === "comedia") 
-         creditos += Math.floor(apre.audiencia / 5);
-      return creditos;   
-    }
-
-    function formatarMoeda(valor) {
-      return new Intl.NumberFormat("pt-BR",
-        { style: "currency", currency: "BRL",
-          minimumFractionDigits: 2 }).format(valor/100);
-    }
-    
-  }
-  faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`
-  faturaStr += `Créditos acumulados: ${creditos} \n`
-  return faturaStr
-}
-
+var Repositorio = require('./repositorio.js')
+var ServicoCalculoFatura = require('./servico.js')
+var gerarFaturaStr = require('./apresentacao.js')
 const faturas = JSON.parse(readFileSync('./faturas.json'))
-const pecas = JSON.parse(readFileSync('./pecas.json'))
-const faturaStr = gerarFaturaStr(faturas, pecas)
+const calc = new ServicoCalculoFatura(new Repositorio())
+const faturaStr = gerarFaturaStr(faturas, calc)
+
 console.log(faturaStr)
